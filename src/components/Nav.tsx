148339156e13a9d89ba0787fe6a04c3158c99e95
Hassansalam4 <atuman4@gmail.com>
2024-12-10 11:54:13 -0500

    working on the nav components

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,10 @@
-const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
-  return (
-    <div>Nav</div>
-  )
-};
+  import { Link } from 'react-router-dom';
+
+  const Nav = () => 
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/SavedCandidates">Saved Candidates</Link>
+      </div>
 
 export default Nav;
